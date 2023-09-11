9197e5d3fd203c1e846819f87a6ee1bc3d6281d9
kmerediith <kymered24@gmail.com>
2023-09-10 22:21:29 -0400

    updated calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+let displayValue = "";
+function calculate(){
+    displayValue = eval(displayValue);
+    document.querySelector('#display').value = displayValue
+}
+
+function handleConcat(value){
+    displayValue+=value; 
+    document.querySelector('#display').value = displayValue
+}
+
+function clearDisplay(){
+    displayValue=""
+    document.querySelector('#display').value = displayValue
+}
